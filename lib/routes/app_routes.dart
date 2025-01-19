0ab2b391f8ad4876f290a65f245cf13add1f6a5b
siiion <siwonjeon234@gmail.com>
2025-01-20 00:04:08 +0900

    FEAT: 홈 화면 UI 구현

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,15 @@
 // 라우팅 관리
 
 import 'package:get/get.dart';
-import 'package:storymate/views/sample_page.dart';
+import 'package:storymate/views/home_page.dart';
 
 class AppRoutes {
   static const HOME = '/';
 
   static final routes = [
-    GetPage(name: HOME, page: () => SamplePage()),
+    GetPage(
+      name: HOME,
+      page: () => HomePage(),
+    ),
   ];
 }
