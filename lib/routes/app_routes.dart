b8161adf47ee7c2aaaff987f6797add85eae528b
siiion <siwonjeon234@gmail.com>
2025-01-20 22:32:34 +0900

    FEAT: 작품 검색 UI 구현

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@
 
 import 'package:get/get.dart';
 import 'package:storymate/views/book_list_page.dart';
+import 'package:storymate/views/book_search_page.dart';
 import 'package:storymate/views/home_page.dart';
 
 class AppRoutes {
@@ -16,6 +17,11 @@ class AppRoutes {
     GetPage(
       name: '/book_list',
       page: () => BookListPage(),
+    ),
+    // 작품 검색 화면
+    GetPage(
+      name: '/book_search',
+      page: () => BookSearchPage(),
     ),
   ];
 }
