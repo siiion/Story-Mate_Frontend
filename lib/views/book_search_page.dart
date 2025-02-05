7e2f42fcf707244a88b8b9e823cd862d4cffef36
siiion <siwonjeon234@gmail.com>
2025-02-05 16:06:35 +0900

    FEAT: 검색 로직 수정(작품 이름, 태그로 검색 가능

--- lib/views/book_search_page.dart
+++ lib/views/book_search_page.dart
@@ -22,15 +22,10 @@ class BookSearchPage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // 뒤로 가기 버튼
                   GestureDetector(
-                    onTap: () {
-                      controller.goBack();
-                    },
-                    child: Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Color(0xff090A0A),
-                    ),
+                    onTap: controller.goBack,
+                    child: Icon(Icons.arrow_back_ios_new,
+                        color: Color(0xff090A0A)),
                   ),
                   Container(
                     width: 320,
@@ -63,12 +58,9 @@ class BookSearchPage extends StatelessWidget {
                               child: Obx(() {
                                 return DropdownButton<String>(
                                   borderRadius: BorderRadius.circular(20),
-                                  isExpanded: false,
                                   value: controller.selectedCategory.value,
-                                  icon: Icon(
-                                    Icons.arrow_drop_down,
-                                    color: Colors.black,
-                                  ),
+                                  icon: Icon(Icons.arrow_drop_down,
+                                      color: Colors.black),
                                   dropdownColor: Colors.white,
                                   style: TextStyle(
                                     color: Colors.black,
@@ -94,7 +86,6 @@ class BookSearchPage extends StatelessWidget {
                           ),
                         ),
                         const SizedBox(width: 10),
-                        // 작품 검색창
                         Expanded(
                           child: TextField(
                             style: TextStyle(
@@ -114,12 +105,9 @@ class BookSearchPage extends StatelessWidget {
                               ),
                               border: InputBorder.none,
                             ),
-                            onChanged: (query) {
-                              controller.searchBooks(query); // 검색 호출
-                            },
+                            onChanged: controller.searchBooks,
                           ),
                         ),
-                        // 검색 버튼
                         Padding(
                           padding: const EdgeInsets.only(right: 10),
                           child: GestureDetector(
@@ -127,10 +115,7 @@ class BookSearchPage extends StatelessWidget {
                               controller.searchBooks(
                                   controller.searchController.text);
                             },
-                            child: Icon(
-                              Icons.search,
-                              color: Colors.white,
-                            ),
+                            child: Icon(Icons.search, color: Colors.white),
                           ),
                         ),
                       ],
@@ -140,78 +125,70 @@ class BookSearchPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20),
-            // 키워드 예시
             Obx(
               () => controller.keywords.isNotEmpty
                   ? Padding(
                       padding: const EdgeInsets.only(left: 20),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Wrap(
-                          spacing: 10,
-                          runSpacing: 10,
-                          children: controller.keywords.map((keyword) {
-                            return GestureDetector(
-                              onTap: () {
-                                controller.searchController.text = keyword;
-                                controller.searchBooks(keyword);
-                              },
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 15, vertical: 5),
-                                decoration: ShapeDecoration(
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    side: BorderSide(
-                                      width: 1,
-                                      color: AppTheme.primaryColor,
-                                    ),
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                ),
-                                child: Text(
-                                  keyword,
-                                  style: TextStyle(
-                                    color: Color(0xFF7C7C7C),
-                                    fontSize: 18,
-                                    fontFamily: 'Jua',
-                                    fontWeight: FontWeight.w400,
-                                  ),
+                      child: Wrap(
+                        spacing: 10,
+                        runSpacing: 10,
+                        children: controller.keywords.map((keyword) {
+                          return GestureDetector(
+                            onTap: () {
+                              controller.searchController.text = keyword;
+                              controller.searchBooks(keyword);
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 5),
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      width: 1, color: AppTheme.primaryColor),
+                                  borderRadius: BorderRadius.circular(30),
                                 ),
                               ),
-                            );
-                          }).toList(),
-                        ),
+                              child: Text(
+                                keyword,
+                                style: TextStyle(
+                                  color: Color(0xFF7C7C7C),
+                                  fontSize: 18,
+                                  fontFamily: 'Jua',
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
                       ),
                     )
                   : SizedBox(),
             ),
             const SizedBox(height: 20),
-            // 검색 결과
+            // 검색 결과 리스트
             Obx(
               () => controller.searchResults.isNotEmpty
                   ? Expanded(
                       child: GridView.builder(
-                        padding: const EdgeInsets.all(10), // 그리드 아이템 여백 설정
+                        padding: const EdgeInsets.all(10),
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount:
-                              MediaQuery.of(context).size.width > 600
-                                  ? 3
-                                  : 2, // 열 개수 (반응형)
-                          crossAxisSpacing: 30, // 열 간격
-                          mainAxisSpacing: 30, // 행 간격
-                          childAspectRatio: 3 / 4, // 아이템 비율 (가로/세로)
+                              MediaQuery.of(context).size.width > 600 ? 3 : 2,
+                          crossAxisSpacing: 30,
+                          mainAxisSpacing: 30,
+                          childAspectRatio: 3 / 4,
                         ),
                         itemCount: controller.searchResults.length,
                         itemBuilder: (context, index) {
-                          final result = controller.searchResults[index];
+                          final book = controller.searchResults[index];
                           return CustomCard(
-                            title: result['title']!,
-                            tags: [],
+                            title: book.title,
+                            tags: book.tags,
+                            coverImage: book.coverImage,
                             onTap: () {
-                              controller.toIntroPage(result['title']!);
+                              controller.toIntroPage(book.title);
                             },
-                            coverImage: '',
                           );
                         },
                       ),
