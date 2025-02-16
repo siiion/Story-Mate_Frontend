1da65920039aa5ca25aa44f06e737b920c1b8f58
siiion <siwonjeon234@gmail.com>
2025-02-16 22:08:53 +0900

    FIX: 결제 위젯 오류 수정

--- lib/views/mypage/payment_page.dart
+++ lib/views/mypage/payment_page.dart
@@ -3,54 +3,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:storymate/components/theme.dart';
 import 'package:storymate/view_models/mypage/payment_controller.dart';
-import 'package:tosspayments_widget_sdk_flutter/model/payment_info.dart';
-import 'package:tosspayments_widget_sdk_flutter/model/payment_widget_options.dart';
-import 'package:tosspayments_widget_sdk_flutter/payment_widget.dart';
 import 'package:tosspayments_widget_sdk_flutter/widgets/agreement.dart';
 import 'package:tosspayments_widget_sdk_flutter/widgets/payment_method.dart';
 
-class PaymentPage extends StatefulWidget {
+class PaymentPage extends StatelessWidget {
   const PaymentPage({super.key});
 
   @override
-  State<PaymentPage> createState() => _PaymentPageState();
-}
-
-class _PaymentPageState extends State<PaymentPage> {
-  final PaymentController controller = Get.put(PaymentController());
-
-  late PaymentWidget _paymentWidget;
-  PaymentMethodWidgetControl? _paymentMethodWidgetControl;
-  AgreementWidgetControl? _agreementWidgetControl;
-  late int amount;
-
-  @override
-  void initState() {
-    super.initState();
-    amount = Get.arguments?['amount'] ?? 500; // 기본값 500원 설정
-
-    _paymentWidget = PaymentWidget(
-      clientKey: "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm",
-      customerKey: "hvDepv1HDm6Dne-B33jj5",
-    );
-
-    _paymentWidget
-        .renderPaymentMethods(
-      selector: 'methods',
-      amount: Amount(value: amount, currency: Currency.KRW, country: "KR"),
-      options: RenderPaymentMethodsOptions(variantKey: "DEFAULT"),
-    )
-        .then((control) {
-      _paymentMethodWidgetControl = control;
-    });
-
-    _paymentWidget.renderAgreement(selector: 'agreement').then((control) {
-      _agreementWidgetControl = control;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final PaymentController controller = Get.put(PaymentController());
+
     return Scaffold(
       backgroundColor: AppTheme.backgroundColor,
       appBar: AppBar(
@@ -87,7 +49,7 @@ class _PaymentPageState extends State<PaymentPage> {
                           ),
                         ),
                         Text(
-                          '$amount원',
+                          '${controller.amount}원',
                           style: TextStyle(
                             fontSize: 18.sp,
                             fontWeight: FontWeight.bold,
@@ -103,35 +65,16 @@ class _PaymentPageState extends State<PaymentPage> {
                     ),
                   ),
                   PaymentMethodWidget(
-                    paymentWidget: _paymentWidget,
+                    paymentWidget: controller.paymentWidget,
                     selector: 'methods',
                   ),
                   AgreementWidget(
-                      paymentWidget: _paymentWidget, selector: 'agreement'),
+                      paymentWidget: controller.paymentWidget,
+                      selector: 'agreement'),
                   const SizedBox(height: 20),
                   Center(
                     child: GestureDetector(
-                      onTap: () async {
-                        final paymentResult =
-                            await _paymentWidget.requestPayment(
-                          paymentInfo: PaymentInfo(
-                            orderId:
-                                'order_${DateTime.now().millisecondsSinceEpoch}',
-                            orderName:
-                                '메시지 ${amount == 500 ? "30개" : "70개"} 충전',
-                          ),
-                        );
-                        if (paymentResult.success != null) {
-                          // 결제 성공 처리
-                          Get.snackbar('결제 성공', '결제가 완료되었습니다.',
-                              snackPosition: SnackPosition.BOTTOM);
-                          Get.back(); // 결제 완료 후 이전 화면으로 이동
-                        } else if (paymentResult.fail != null) {
-                          // 결제 실패 처리
-                          Get.snackbar('결제 실패', '결제에 실패했습니다.',
-                              snackPosition: SnackPosition.BOTTOM);
-                        }
-                      },
+                      onTap: () => controller.requestPayment(),
                       child: Container(
                         width: 300.w,
                         height: 60.h,
