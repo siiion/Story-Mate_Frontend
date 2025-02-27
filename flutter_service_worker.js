e44fd6400a8115bf1d91238af46c195750d82452
siiion <112245253+siiion@users.noreply.github.com>
2025-02-27 14:23:46 +0000

    Deploying to gh-pages from @ siiion/Story-Mate_Frontend@d059ec426542a68feb3db6a051ef04720a47548f 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -111,7 +111,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "d1a7fb888edaad90b17db5aaeaa9be4f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "2f5dac4dbd87f1b9f4139366feb315e3"};
+"flutter_bootstrap.js": "7bbf0d61c0f7f1bec311c48a84000335"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
